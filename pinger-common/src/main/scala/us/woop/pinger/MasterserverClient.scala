1a3addaa600b41e6eb1865a76f3611c57f8256e1
William Narmontas <william@vynar.com>
2014-03-29 18:44:23 +0000

    Refactored again - have: client (parsing the raw data, performing the pings, network stack, etc); common (case classes and the like + a masterserver client); service (the logic that makes use of the client - pure akka)

---
+++ pinger-common/src/main/scala/us/woop/pinger/MasterserverClient.scala
@@ -0,0 +1,48 @@
+package us.woop.pinger
+
+import java.net.Socket
+import java.io.{BufferedReader, InputStreamReader, DataOutputStream}
+import scala.util.Try
+import scala.util.control.NonFatal
+
+/** 01/02/14 */
+object MasterserverClient {
+
+  val serverRegex = """^addserver (\d+\.\d+\.\d+\.\d+) (\d+)$""".r
+  val parseServerLine: PartialFunction[String, (String, Int)] =
+    { case serverRegex(host, port) => (host, port.toInt) }
+
+  val sauerMasterserver = ("sauerbraten.org", 28787)
+
+  def getServers(masterServer: (String, Int)): Set[(String, Int)] = {
+    val socket = new Socket(masterServer._1, masterServer._2)
+    try {
+      val dataOutputStream = new DataOutputStream(socket.getOutputStream)
+      try {
+        val inputStreamReader = new InputStreamReader(socket.getInputStream)
+        try {
+          val dataInputStream = new BufferedReader(inputStreamReader)
+          try {
+            dataOutputStream.writeBytes("list\n")
+            def getLine = Try(Option(dataInputStream.readLine)).toOption.flatten.filterNot{_ == '\0'.toString}
+            val contents = Stream.continually(getLine).takeWhile(_ != None).flatten.toList
+            try {
+              (contents map parseServerLine).toSet
+            } catch {
+              case NonFatal(e) => throw new MatchError(s"Failed to parse contents due to '$e' - contents: '$contents'")
+            }
+          } finally {
+            dataInputStream.close()
+          }
+        } finally {
+          inputStreamReader.close()
+        }
+      } finally {
+        dataOutputStream.close()
+      }
+    } finally {
+      socket.close()
+    }
+  }
+
+}
