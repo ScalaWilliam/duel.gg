b93d4af50a8295e54fe412166bf107eb01894159
William Narmontas <william@vynar.com>
2014-03-30 18:07:49 +0100

    Factoring out the non-Akka code into its own trait

--- pinger-service/src/main/scala/us/woop/pinger/PingerService.scala
+++ pinger-service/src/main/scala/us/woop/pinger/PingerService.scala
@@ -6,90 +6,29 @@ import scala.concurrent.duration._
 import us.woop.pinger.PingerClient.{Ready, Ping}
 import akka.event.LoggingReceive
 
-class PingerService(injectPingerClient: Option[ActorRef] = None) extends Actor with ActorLogging with Stash {
+class PingerService(injectPingerClient: Option[ActorRef] = None) extends Actor with ActorLogging with Stash with PingerServiceMixin {
 
   import PingerServiceData._
 
-  val settings = PingerServiceSettings(context.system)
+  lazy val settings = PingerServiceSettings(context.system)
+
   def this() = this(None)
+
   def this(injectedPingerClient: ActorRef) = this(Option(injectedPingerClient))
-  val pingerClient = injectPingerClient.getOrElse{context.actorOf(Props(classOf[PingerClient], self), name = "pingerClient")}
 
-  type MinimumRate = Option[FiniteDuration]
-
-  val subscriptions = scala.collection.mutable.Map[(Server, ActorRef), MinimumRate]()
-  val schedules = scala.collection.mutable.Map[Server, Cancellable]()
-  val firehose = scala.collection.mutable.Set[ActorRef]()
-
-  /** A view of subscribers per each server based on subscriptions above **/
-  def serversSubscribers =
-    subscriptions.groupBy{_._1._1}.mapValues{_.keys.map{_._2}}
-
-  /** *
-    * Calculate a ping interval for a server. This is pretty much a map.
-    * If there are no subscriptions it returns None,
-    * If all the subscriptions lack a return rate we give it the default
-    * Else we provide the minimum rate out of those that are set.
-    * @param server The server to play with.
-    * @return Optional finite duration
-    */
-  def pingInterval(server: Server): Option[FiniteDuration]= {
-    val requiredRates = for { ((`server`, _), requiredRate) <- subscriptions } yield requiredRate
-    requiredRates.toList match {
-      case Nil => None
-      case rates => Option((for {
-        requiredRate <- rates
-        duration <- requiredRate
-      } yield duration).reduceLeftOption((x, y) => if (x > y) y else x).getOrElse(settings.defaultPingInterval))
-    }
+  val pingerClient = injectPingerClient.getOrElse {
+    context.actorOf(Props(classOf[PingerClient], self), name = "pingerClient")
   }
 
   import context.dispatcher
-  /** Add a schedule if it's not already set **/
-  def scheduleSafely(server: Server, initially: FiniteDuration = initialDelay) =
-    for {
-      interval <- pingInterval(server)
-      ping = Ping((server.ip, server.port))
-    } schedules.getOrElseUpdate(server, {
-      log.debug("Starting schedule for {} at interval {}", server, interval)
-      context.system.scheduler.schedule(initially, interval, pingerClient, ping)
-    })
 
-  val initialDelay = settings.subscribeToPingDelay
+  def schedule(a: FiniteDuration, b: FiniteDuration, d: Any) =
+    context.system.scheduler.schedule(a, b, pingerClient, d)
 
   override def postStop() {
-    for { (server, schedule) <- schedules } {
+    for {(server, schedule) <- schedules} {
       log.debug("Canceling schedule {} for {}", schedule, server)
       schedule.cancel()
-    }
-  }
-  def removeSubscription(subscription: (Server, ActorRef)) {
-    val server = subscription._1
-    val oldInterval = pingInterval(server)
-
-    /** Restart schedule if interval is higher **/
-    /** Do nothing if interval does not change **/
-    subscriptions remove subscription
-
-    pingInterval(server) match {
-      case None =>
-        for {
-          schedule <- schedules remove server
-        } {
-          log.debug("Canceling schedule for {}", schedule, server)
-          schedule.cancel()
-        }
-
-      case Some(newIntervalDuration) =>
-        for {
-          oldIntervalDuration <- oldInterval
-          if newIntervalDuration > oldIntervalDuration
-          schedule <- schedules remove server
-        } {
-          log.debug("Increasing {} schedule interval from {} to {} ", server, oldIntervalDuration, newIntervalDuration)
-          schedule.cancel()
-          scheduleSafely(server)
-        }
     }
   }
 
@@ -105,33 +44,14 @@ class PingerService(injectPingerClient: Option[ActorRef] = None) extends Actor w
           log.debug("Requesting subscription {} at rate {}", subscription, requiredInterval)
       }
 
-      /** Cancel current schedule if the demanded rate is higher **/
-      val oldInterval = pingInterval(server)
-      subscriptions += subscription -> requiredInterval
-      val decreasedSchedule = for {
-        intervalDuration <- requiredInterval
-        oldIntervalDuration <- oldInterval
-        if intervalDuration < oldIntervalDuration
-        schedule <- schedules remove server
-      } yield {
-        log.debug("Decreasing {} schedule frequency from {} to {}", server, oldIntervalDuration, intervalDuration)
-        schedule.cancel()
-        scheduleSafely(server)
-      }
+      pushSubscription(subscription, requiredInterval)
 
-      decreasedSchedule match {
-        case None =>
-          // not done anything to affect any schedules - so we're starting anew
-          log.debug("No current schedules found - starting one for {} at rate {}", server, pingInterval(server))
-          scheduleSafely(server)
-          context.watch(sender())
-        case _ =>
-      }
+      context.watch(sender())
 
     /**
-      * Unsubscribe the subscriber from the server.
-      * If the server has no more subscribers, it gets removed.
-      */
+     * Unsubscribe the subscriber from the server.
+     * If the server has no more subscribers, it gets removed.
+     */
     case Unsubscribe(server) =>
       val subscription = server -> sender()
       log.debug("Unsubscribe request for {}", subscription)
@@ -140,10 +60,10 @@ class PingerService(injectPingerClient: Option[ActorRef] = None) extends Actor w
     case Terminated(subscriber) =>
       log.debug("Watched subscriber {} has terminated", subscriber)
       val subscriptionsToRemove = for {
-        subscription @ (_, `subscriber`) <- subscriptions.keys.toList
+        subscription@(_, `subscriber`) <- subscriptions.keys.toList
       } yield subscription
       subscriptionsToRemove foreach removeSubscription
-      if ( firehose remove subscriber ) {
+      if (firehose remove subscriber) {
         log.debug("{} requested firehose access")
       }
 
@@ -153,13 +73,13 @@ class PingerService(injectPingerClient: Option[ActorRef] = None) extends Actor w
       firehose += client
       context.watch(client)
 
-    case Unfirehose  =>
+    case Unfirehose =>
       log.debug("{} requested to revoke firehose access")
       firehose -= sender()
 
-    case message @ Tuple2((ip: String, port: Int), payload: Any) if sender() == pingerClient =>
+    case message@Tuple2((ip: String, port: Int), payload: Any) if sender() == pingerClient =>
       val server = Server(ip, port)
-      val message = SauerbratenPong (
+      val message = SauerbratenPong(
         unixTime = System.currentTimeMillis() / 1000L,
         host = (ip, port),
         payload = payload
@@ -172,6 +92,7 @@ class PingerService(injectPingerClient: Option[ActorRef] = None) extends Actor w
         hose <- firehose
       } hose ! message
   }
+
   def receive = {
     case Ready(_) =>
       log.debug("{} is now ready", sender())
---
+++ pinger-service/src/main/scala/us/woop/pinger/PingerServiceMixin.scala
@@ -0,0 +1,116 @@
+package us.woop.pinger
+
+import us.woop.pinger.PingerServiceData.Server
+import akka.actor.{ActorLogging, Cancellable, ActorRef}
+import scala.concurrent.duration.FiniteDuration
+import us.woop.pinger.PingerClient.Ping
+
+trait PingerServiceMixin {
+  this: ActorLogging =>
+
+  def settings: PingerServiceSettingsImpl
+
+  /** A view of subscribers per each server based on subscriptions above **/
+  def serversSubscribers =
+    subscriptions.groupBy {
+      _._1._1
+    }.mapValues {
+      _.keys.map {
+        _._2
+      }
+    }
+
+  lazy val initialDelay = settings.subscribeToPingDelay
+
+  type MinimumRate = Option[FiniteDuration]
+  val subscriptions = scala.collection.mutable.Map[(Server, ActorRef), MinimumRate]()
+  val schedules = scala.collection.mutable.Map[Server, Cancellable]()
+  val firehose = scala.collection.mutable.Set[ActorRef]()
+
+  /** Add a schedule if it's not already set **/
+  def scheduleSafely(server: Server, initially: FiniteDuration = initialDelay) =
+    for {
+      interval <- pingInterval(server)
+      ping = Ping((server.ip, server.port))
+    } schedules.getOrElseUpdate(server, {
+      log.debug("Starting schedule for {} at interval {}", server, interval)
+      schedule(initially, interval, ping)
+    })
+
+  def schedule(a: FiniteDuration, b: FiniteDuration, d: Any): Cancellable
+
+
+  /** *
+    * Calculate a ping interval for a server. This is pretty much a map.
+    * If there are no subscriptions it returns None,
+    * If all the subscriptions lack a return rate we give it the default
+    * Else we provide the minimum rate out of those that are set.
+    * @param server The server to play with.
+    * @return Optional finite duration
+    */
+  def pingInterval(server: Server): Option[FiniteDuration] = {
+    val requiredRates = for {((`server`, _), requiredRate) <- subscriptions} yield requiredRate
+    requiredRates.toList match {
+      case Nil => None
+      case rates => Option((for {
+        requiredRate <- rates
+        duration <- requiredRate
+      } yield duration).reduceLeftOption((x, y) => if (x > y) y else x).getOrElse(settings.defaultPingInterval))
+    }
+  }
+
+  def removeSubscription(subscription: (Server, ActorRef)) {
+    val server = subscription._1
+    val oldInterval = pingInterval(server)
+
+    /** Restart schedule if interval is higher **/
+    /** Do nothing if interval does not change **/
+    subscriptions remove subscription
+
+    pingInterval(server) match {
+      case None =>
+        for {
+          schedule <- schedules remove server
+        } {
+          log.debug("Canceling schedule for {}", schedule, server)
+          schedule.cancel()
+        }
+
+      case Some(newIntervalDuration) =>
+        for {
+          oldIntervalDuration <- oldInterval
+          if newIntervalDuration > oldIntervalDuration
+          schedule <- schedules remove server
+        } {
+          log.debug("Increasing {} schedule interval from {} to {} ", server, oldIntervalDuration, newIntervalDuration)
+          schedule.cancel()
+          scheduleSafely(server)
+        }
+    }
+  }
+
+  def pushSubscription(subscription: (Server, ActorRef), requiredInterval: Option[FiniteDuration]) {
+    val server = subscription._1
+    /** Cancel current schedule if the demanded rate is higher **/
+    val oldInterval = pingInterval(server)
+    subscriptions += subscription -> requiredInterval
+    val decreasedSchedule = for {
+      intervalDuration <- requiredInterval
+      oldIntervalDuration <- oldInterval
+      if intervalDuration < oldIntervalDuration
+      schedule <- schedules remove server
+    } yield {
+      log.debug("Decreasing {} schedule frequency from {} to {}", server, oldIntervalDuration, intervalDuration)
+      schedule.cancel()
+      scheduleSafely(server)
+    }
+
+    decreasedSchedule match {
+      case None =>
+        // not done anything to affect any schedules - so we're starting anew
+        log.debug("No current schedules found - starting one for {} at rate {}", server, pingInterval(server))
+        scheduleSafely(server)
+      case _ =>
+    }
+  }
+}
