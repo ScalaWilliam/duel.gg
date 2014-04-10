b4191ffb9882da2ba4d1a67108d8e2726dcc1166
William Narmontas <william@vynar.com>
2014-04-10 22:38:22 +0100

    Even more awesome stuff. Sorted LevelDB implementation with read and write formats. Working nicely and tidely. Added indices. This is uber awesome. First time doing database stuff at such low level!

--- pinger-client/src/main/scala/us/woop/pinger/client/PersistRawData.scala
+++ pinger-client/src/main/scala/us/woop/pinger/client/PersistRawData.scala
@@ -4,25 +4,22 @@ import java.io.File
 import akka.actor.ActorDSL._
 import akka.actor.ActorLogging
 import scala.util.control.NonFatal
-import us.woop.pinger.data.{PingPongProcessor, PersistRawData}
-import PersistRawData.DatabaseUseException
-import akka.util.ByteStringBuilder
-import PingPongProcessor.ReceivedBytes
 import org.iq80.leveldb._
 import org.fusesource.leveldbjni.JniDBFactory._
 import java.nio.ByteOrder
 import org.iq80.leveldb
-
+import us.woop.pinger.data.PingPongProcessor.Server
 
 
 class PersistRawData(target: File) extends Act with ActorLogging {
 
+  import us.woop.pinger.data.{PingPongProcessor, PersistRawData}
+  import PersistRawData.DatabaseUseException
+  import akka.util.ByteStringBuilder
+  import PingPongProcessor.ReceivedBytes
   var db: DB = _
-  var lastTime: Long = _
-  def now = System.currentTimeMillis
 
   whenStarting {
-    lastTime = now
     try {
       val options = {
         val options = new Options()
@@ -30,6 +27,11 @@ class PersistRawData(target: File) extends Act with ActorLogging {
       }
       log.info("Opening database {}", target)
       db = factory.open(target, options)
+      implicit val byteOrdering = ByteOrder.BIG_ENDIAN
+      val indexIndexKey = new ByteStringBuilder().putBytes("server.index".getBytes).putLong(0L).result().toArray
+      if ( db.get(indexIndexKey) == null ) {
+        db.put(indexIndexKey, Array[Byte]())
+      }
     } catch {
       case NonFatal(e) =>
         throw new DatabaseUseException(e)
@@ -45,15 +47,24 @@ class PersistRawData(target: File) extends Act with ActorLogging {
 
   val wo = new leveldb.WriteOptions { sync(true) }
 
+  def ensureFirstIndex(server: Server, key: Array[Byte]) {
+    val ipBytes = server.ip.ip.split("\\.").map{_.toInt.toByte}
+    implicit val byteOrdering = ByteOrder.BIG_ENDIAN
+    val indexKey = new ByteStringBuilder().putBytes("server.index".getBytes).putBytes(ipBytes).putInt(server.port).result().toArray
+    // TODO make LevelDB into a scala mutable Map!!! :-)
+    if (db.get(indexKey) == null) {
+      db.put(indexKey, key)
+    }
+  }
+
   def persist(msg: PingPongProcessor.ReceivedBytes) {
-    val currentTime = now
-    lastTime = if ( lastTime < currentTime ) currentTime else lastTime + 1
     try {
       val key = {
         val ipBytes = msg.server.ip.ip.split("\\.").map{_.toInt.toByte}
         implicit val byteOrdering = ByteOrder.BIG_ENDIAN
-        new ByteStringBuilder().putLong(lastTime).putBytes(ipBytes).putInt(msg.server.port).result().toArray
+        new ByteStringBuilder().putBytes("msg".getBytes).putBytes(ipBytes).putInt(msg.server.port).putLong(msg.time).result().toArray
       }
+      ensureFirstIndex(msg.server, key)
       db.put(key, msg.message.toArray, wo)
     } catch {
       case NonFatal(e) => throw new DatabaseUseException(e)
