463bfeedb6c70214eb0211af09cafc3c44b22c6e
Vykintas Narmontas <me@vynar.com>
2015-07-30 19:01:01 +0100

    Using reagent now

--- frontend/project.clj
+++ frontend/project.clj
@@ -1,45 +1,91 @@
-(defproject duelggfrontend "3.0"
-  :url "http://duel.gg"
+(defproject myproject "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :source-paths ["src/clj" "src/cljs"]
+
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [ring/ring-core "1.4.0"]
-                 [ring/ring-json "0.3.1"]
+                 [ring-server "0.4.0"]
+                 [cljsjs/react "0.13.3-1"]
+                 [reagent "0.5.0"]
+                 [reagent-forms "0.5.5"]
+                 [reagent-utils "0.1.5"]
+                 [ring "1.4.0"]
+                 [ring/ring-defaults "0.1.5"]
+                 [prone "0.8.2"]
                  [compojure "1.4.0"]
-                 [clj-http "2.0.0"]
-                 [org.clojure/data.json "0.2.6"]
-                 [org.clojure/clojurescript "0.0-3308"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [cljs-http "0.1.35"]
-                 [org.omcljs/om "0.9.0"]
-                 [figwheel "0.3.7"]]
+                 [hiccup "1.0.5"]
+                 [environ "1.0.0"]
+                 [org.clojure/clojurescript "0.0-3308" :scope "provided"]
+                 [secretary "1.2.3"]]
 
-  :plugins [[lein-cljsbuild "1.0.6"]
-            [lein-ring "0.9.6"]
-            [lein-pdo "0.1.1"]
-          [lein-release "1.0.5"]
-  [lein-figwheel "0.3.3"]]
+  :plugins [[lein-environ "1.0.0"]
+            [lein-asset-minifier "0.2.2"]
+            [lein-ring "0.9.6"]]
 
-  :aliases {"up" ["pdo" "cljsbuild" "auto" "dev," "ring" "server-headless"]}
-  
-  :lein-release {:scm :git}
+  :ring {:handler myproject.handler/app
+         :uberwar-name "myproject.war"}
 
   :min-lein-version "2.5.0"
-  :uberjar-name "frontend-standalone.jar"
 
-  :ring {:handler frontend.core/app
-         :init frontend.core/init}
+  :uberjar-name "myproject.jar"
 
-  :source-paths ["src/clj"]
+  :main myproject.server
 
-  :cljsbuild {:builds [{:id "dev"
-                        :source-paths ["src/cljs"]
-                        :compiler {:output-to "resources/public/js/app.js"
-                                   :output-dir "resources/public/js/out"
-                                   :optimizations :none
-                                   :source-map true}}
-                       {:id "release"
-                        :source-paths ["src/cljs"]
-                        :compiler {:output-to "resources/public/js/app.js"
-                                   :optimizations :advanced
-                                   :pretty-print false
-                                   :preamble ["react/react.min.js"]
-                                   :externs ["react/externs/react.js"]}}]})
+  :clean-targets ^{:protect false} [:target-path
+                                    [:cljsbuild :builds :app :compiler :output-dir]
+                                    [:cljsbuild :builds :app :compiler :output-to]]
+
+  :minify-assets
+  {:assets
+    {"resources/public/css/site.min.css" "resources/public/css/site.css"}}
+
+  :cljsbuild {:builds {:app {:source-paths ["src/cljs"]
+                             :compiler {:output-to     "resources/public/js/app.js"
+                                        :output-dir    "resources/public/js/out"
+                                        :asset-path   "js/out"
+                                        :optimizations :none
+                                        :pretty-print  true}}}}
+
+  :profiles {:dev {:repl-options {:init-ns myproject.repl
+                                  :nrepl-middleware []}
+
+                   :dependencies [[ring/ring-mock "0.2.0"]
+                                  [ring/ring-devel "1.4.0"]
+                                  [lein-figwheel "0.3.7"]
+                                  [org.clojure/tools.nrepl "0.2.10"]
+                                  [pjstadig/humane-test-output "0.7.0"]]
+
+                   :source-paths ["env/dev/clj"]
+                   :plugins [[lein-figwheel "0.3.7"]
+                             [lein-cljsbuild "1.0.6"]]
+
+                   :injections [(require 'pjstadig.humane-test-output)
+                                (pjstadig.humane-test-output/activate!)]
+
+                   :figwheel {:http-server-root "public"
+                              :server-port 3449
+                              :nrepl-port 7002
+                              :css-dirs ["resources/public/css"]
+                              :ring-handler myproject.handler/app}
+
+                   :env {:dev true}
+
+                   :cljsbuild {:builds {:app {:source-paths ["env/dev/cljs"]
+                                              :compiler {:main "myproject.dev"
+                                                         :source-map true}}
+}
+}}
+
+             :uberjar {:hooks [leiningen.cljsbuild minify-assets.plugin/hooks]
+                       :env {:production true}
+                       :aot :all
+                       :omit-source true
+                       :cljsbuild {:jar true
+                                   :builds {:app
+                                             {:source-paths ["env/prod/cljs"]
+                                              :compiler
+                                              {:optimizations :advanced
+                                               :pretty-print false}}}}}})
