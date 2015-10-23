d858d221b1e7f039b35438b0818f030cea504b97
Vykintas Narmontas <me@vynar.com>
2015-10-23 14:46:15 +0100

    Add most ecent changes (from August 2015)

--- frontend/src/clj/myproject/server.clj
+++ frontend/src/clj/myproject/server.clj
@@ -1,9 +1,11 @@
 (ns myproject.server
   (:require [myproject.handler :refer [app]]
             [environ.core :refer [env]]
-            [ring.adapter.jetty :refer [run-jetty]])
+            [ring.adapter.jetty :refer [run-jetty]]
+            [org.httpkit.server :refer [run-server]]
+            )
   (:gen-class))
 
  (defn -main [& args]
    (let [port (Integer/parseInt (or (env :port) "3000"))]
-     (run-jetty app {:port port :join? false})))
+     (run-server app {:port port :join? false})))
