92e23f585be281771e27c7f4d4e15cdddce06a7f
William Narmontas <william@scalawilliam.com>
2015-10-24 12:21:25 +0100

    Clean up pinger-service API, turn it into an SSE-only thing. Add a SSE consumer via akka-sse

---
+++ games-api/app/modules/UpstreamGames.scala
@@ -0,0 +1,38 @@
+package modules
+
+import akka.http.scaladsl.Http.OutgoingConnection
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{FlattenStrategy, Flow, Source}
+import akka.stream.stage.{Context, PushStage, SyncDirective, TerminationDirective}
+import de.heikoseeberger.akkasse.ServerSentEvent
+
+import scala.concurrent.Future
+
+object UpstreamGames {
+
+  type HttpConnection = Flow[HttpRequest, HttpResponse, Future[OutgoingConnection]]
+
+  def getPermanentStream(httpConnection: HttpConnection)(httpRequest: HttpRequest)(implicit actorMaterializer: ActorMaterializer): Source[ServerSentEvent, Unit] =
+    Source.repeat {
+      Source.single(httpRequest)
+        .via(httpConnection)
+        .transform(() => finishOnFailure)
+        .mapAsync(1)(Unmarshal(_).to[Source[ServerSentEvent, Any]])
+        .flatten(FlattenStrategy.concat)
+        .transform(() => finishOnFailure)
+    }.flatten(FlattenStrategy.concat)
+
+  def finishOnFailure[T] = new PushStage[T, T] {
+    override def onPush(element: T, ctx: Context[T]): SyncDirective = {
+      ctx.push(element)
+    }
+
+    override def onUpstreamFailure(cause: Throwable, ctx: Context[T]): TerminationDirective = {
+      ctx.finish()
+    }
+  }
+
+
+}
--- pinger-service/app/controllers/Main.scala
+++ pinger-service/app/controllers/Main.scala
@@ -7,7 +7,8 @@ import models.games.{Ctfs, Duels, GamesIndex, GamesManager}
 import models.pinger.PingerService
 import models.servers.ServerManager
 import play.api.libs.EventSource
-import play.api.libs.iteratee.Iteratee
+import play.api.libs.EventSource.Event
+import play.api.libs.iteratee.{Enumerator, Iteratee}
 import play.api.mvc.{Action, Controller, WebSocket}
 
 import scala.concurrent.ExecutionContext
@@ -23,12 +24,40 @@ class Main @Inject()
     Ok(views.html.index())
   }
 
-  def servers = Action {
-    Ok(serverProvider.servers)
+  private def allGamesEnum = Enumerator.enumerate(
+    traversable = gamesManager.games.asCombined.games.map(_.fold(scd =>
+      Event(
+        id = Option(scd.startTimeText),
+        name = Option("duel"),
+        data = scd.toJson), scc =>
+      Event(
+        id = Option(scc.startTimeText),
+        name = Option("ctf"),
+      data = scc.toJson
+      )))
+  )
+
+  def allGames = Action {
+    implicit req =>
+      Ok.feed(content = allGamesEnum).as("text/event-stream")
   }
 
-  def range(from: Long, to: Long) = Action {
-    Ok(gamesManager.games.asCombined.range(from, to))
+  def allGamesAndNew = Action {
+    implicit req =>
+      Ok.feed(
+        content = allGamesEnum.andThen(pingerService.enumerator)
+      ).as("text/event-stream")
+  }
+
+  def newGames = Action {
+    implicit req =>
+      Ok.feed(
+        content = pingerService.enumerator
+      ).as("text/event-stream")
+  }
+
+  def servers = Action {
+    Ok(serverProvider.servers)
   }
 
   def recent = Action {
@@ -43,38 +72,6 @@ class Main @Inject()
   def deleteServer(serverId: String) = Action {
     serverProvider.deleteServer(serverId)
     Ok(serverId)
-  }
-
-  def gamesIndex = Action {
-    Ok(GamesIndex(
-      ctfs = gamesManager.games.ctfs.valuesIterator.map(_.startTime).toList.sorted.map(_.toString),
-      duels = gamesManager.games.duels.valuesIterator.map(_.startTime).toList.sorted.map(_.toString)
-    ))
-  }
-
-  def duels(from: Long, to: Long) = Action {
-    Ok(Duels(
-      gamesManager.games.duels.valuesIterator.filter {
-        game => game.startTime >= from && game.startTime <= to
-      }.toList.sortBy(_.startTime)
-    ))
-  }
-
-  def ctfs(from: Long, to: Long) = Action {
-    Ok(Ctfs(
-      gamesManager.games.ctfs.valuesIterator.filter {
-        game => game.startTime >= from && game.startTime <= to
-      }.toList.sortBy(_.startTime)
-    ))
-  }
-
-  def gamesStreamSse = Action {
-    implicit req =>
-      Ok.feed(pingerService.enumerator &> EventSource()).as("text/event-stream")
-  }
-
-  def gamesStreamWs = WebSocket.using[String] { request =>
-    (Iteratee.isEmpty, pingerService.enumerator)
   }
 
   def currentStatus = Action {
