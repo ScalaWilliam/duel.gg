57172a5dc107e86066d4307ed75d5b96e0a70679
William Narmontas <william@scalawilliam.com>
2015-10-25 19:35:28 +0000

    * Add functional user registration - Frontend & API. * Fix player registration rule that allows duplicate Googles.

--- api/app/controllers/AuthenticationApi.scala
+++ api/app/controllers/AuthenticationApi.scala
@@ -1,12 +1,17 @@
 package controllers
 
-import java.time.LocalDateTime
+import java.time.{ZonedDateTime, LocalDateTime}
 import javax.inject._
+import play.api.data._
+import play.api.data.Forms._
 
 import com.maxmind.geoip.LookupService
-import controllers.authentication.{AuthenticationUser, CookieParser, GoogleToken}
-import play.api.Configuration
+import controllers.authentication.{AuthenticationUser, CookieParser, GoogleToken, SignedParser}
+import gg.duel.uservice.player.RegisterPlayer
+import modules.PlayerClanManager
+import play.api.{Logger, Configuration}
 import play.api.libs.Jsonp
+import play.api.libs.json.Json
 import play.api.libs.ws.WSClient
 import play.api.mvc.{Action, Controller, RequestHeader}
 
@@ -14,7 +19,8 @@ import scala.async.Async
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class AuthenticationApi @Inject()(wSClient: WSClient, configuration: Configuration)(implicit executionContext: ExecutionContext) extends Controller {
+class AuthenticationApi @Inject()(wSClient: WSClient, configuration: Configuration,
+                                  playerClanManager: PlayerClanManager)(implicit executionContext: ExecutionContext) extends Controller {
   lazy val cp = CookieParser(salt = salt)
   val defaultExpiryDays = configuration.getInt("gg.duel.api.auth.default-expiry-days").get
   val expirySoonDays = configuration.getInt("gg.duel.api.auth.expiry-soon-days").get
@@ -22,7 +28,59 @@ class AuthenticationApi @Inject()(wSClient: WSClient, configuration: Configurati
   val aud = configuration.getString("gg.duel.api.auth.aud").get
   val salt = configuration.getString("gg.duel.api.auth.salt").get
   val lookupService = new LookupService("resources/GeoLiteCityv6.dat")
-  lookupService
+  val signedParser = SignedParser(salt = salt)
+  val userRegistrationForm = Form(
+    mapping(
+      "id" -> text,
+      "nickname" -> text,
+      "authToken" -> text
+    )(UserRegistration.apply)(UserRegistration.unapply)
+  )
+
+  def register = Action.async { implicit req =>
+    Async.async {
+      userRegistrationForm.bindFromRequest().value match {
+        case None => BadRequest("Could not find a good form")
+        case Some(form) =>
+          form.validAuthToken match {
+            case Some(auth) =>
+              Async.await {
+                playerClanManager.registerPlayer(
+                  registerPlayer = RegisterPlayer(
+                    id = form.id,
+                    google = auth.google,
+                    nickname = form.nickname,
+                    countryCode = auth.countryCode.getOrElse("UN"),
+                    startTime = Option.empty
+                  ),
+                  registerTime = ZonedDateTime.now()
+                )
+              } match {
+                case Left(reason) =>
+                  BadRequest(s"Failed to register due to: $reason")
+                case Right(playerAndClan) =>
+                  Ok(Json.toJson(playerAndClan))
+              }
+            case None =>
+              BadRequest("Invalid auth token.")
+          }
+      }
+    }
+  }
+
+  case class UserRegistration(id: String, nickname: String, userToken: String) {
+    def validAuthToken = {
+      PartialFunction.condOpt(userToken) {
+        case signedParser.fromSignedString(authenticationUser) if !authenticationUser.isExpired =>
+          authenticationUser
+      }
+    }
+  }
+
+  def requestCountryCode(requestHeader: RequestHeader): Option[String] = {
+    Option(lookupService.getLocationV6(requestHeader.remoteAddress))
+      .flatMap(location => Option(location.countryCode))
+  }
 
   def validateGoogle(requestHeader: RequestHeader): Future[Option[AuthenticationUser]] = {
     Async.async {
@@ -35,8 +93,7 @@ class AuthenticationApi @Inject()(wSClient: WSClient, configuration: Configurati
             case GoogleToken.fromJson(googleToken) if googleToken.isValid(expectedAud = aud) =>
               AuthenticationUser(
                 google = googleToken.email,
-                countryCode = Option(lookupService.getLocationV6(requestHeader.remoteAddress))
-                  .flatMap(location => Option(location.countryCode)),
+                countryCode = requestCountryCode(requestHeader),
                 id = Option.empty,
                 expires = LocalDateTime.now().plusDays(7)
               )
