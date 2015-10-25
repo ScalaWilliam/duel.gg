0e4628080aeea2961c169fd09362c3e3e4bbc402
William Narmontas <william@scalawilliam.com>
2015-10-25 17:41:35 +0000

    Ok, fair enough.

--- frontend/www/news/sync.php
+++ frontend/www/news/sync.php
@@ -8,15 +8,9 @@ require("../basex.inc.php");
 file_put_contents("tumblr.rss.xml", file_get_contents("http://duelgg.tumblr.com/rss"));
 file_put_contents("tumblr.api-read.xml", file_get_contents("http://duelgg.tumblr.com/api/read"));
 
-$read = simplexml_load_file("tumblr.api-read.xml");
-foreach($read->posts->post as $post) {
-    if ( isset($post['url-with-slug']) && preg_match('/.*\/post\/[0-9]+\/([a-zA-Z0-9-]+)$/', $post['url-with-slug'], $matches)
-    && isset($post['url']) && preg_match('/^http:\/\/duelgg.tumblr.com\/post\/[0-9]+$/', $post['url'], $_) ) {
-        $f = $matches[1];
-        @mkdir($f);
-        @file_put_contents($f . "/index.php", "<" . "?php \$tumblr_item = '".$post['url']."'; require('../view_item.php');");
-        if ( strpos($gitignore_contents, $f) === false ) {
-            $gitignore_contents .= "\n$f/\n";
-        }
-    }
-}
+header("Content-Type: application/xml");
+?>
+<XML>
+    <RSS><?php echo file_get_contents("tumblr.rss.xml"); ?></RSS>
+    <FEED><?php echo file_get_contents("tumblr.api-read.xml"); ?></FEED>
+</XML>
