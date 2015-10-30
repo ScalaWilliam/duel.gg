ce848f0a4347183c367bb928eddcc549976b9910
William Narmontas <william@scalawilliam.com>
2015-10-30 22:31:10 +0000

    Update /game/ endpoint for frontend

--- frontend/www/game/index.php
+++ frontend/www/game/index.php
@@ -4,7 +4,7 @@ require("../render.inc.php");
     <div id="content" style="width:60em;margin-left:auto;margin-right:auto;">
 
             <?php
-            $game = json_decode(file_get_contents("http://alfa.duel.gg/api/game/".rawurlencode($_GET['id'])."/"), true);
+            $game = json_decode(file_get_contents("http://api.duel.gg/game/".rawurlencode($_GET['id'])."/"), true);
             require_once("../render_game.inc.php");
             render_duel($game);
             ?>
