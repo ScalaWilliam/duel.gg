2c96223ba5966c79cc2cacf25fb3863d71fc1628
William Narmontas <william@scalawilliam.com>
2015-10-30 22:57:43 +0000

    Add SYNC for alfa.

--- frontend/www/sync/index.php
+++ frontend/www/sync/index.php
@@ -2,5 +2,17 @@
 if ( !isset($_SERVER['SYNC_KEY']) ) die("no SYNC_KEY set");
 if ( $_GET['sync-key'] !== $_SERVER['SYNC_KEY'] ) die("provided sync key is invalid");
 
-system("git checkout master");
-system("git pull");
+if ( $_SERVER['HTTP_POST'] == "alfa.duel.gg" ) {
+    $post_body = file_get_contents('php://input');
+    $json = json_decode($post_body, true);
+    if ( strpos($post_body, "frontend") !== false ) {
+        $sha = $json['after'];
+        if (ctype_alnum($sha)) {
+            system("git fetch");
+            system("git checkout $sha");
+        }
+    }
+} else {
+    system("git checkout master");
+    system("git pull");
+}
