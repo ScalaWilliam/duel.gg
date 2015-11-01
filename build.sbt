b5b427e7179b67f8c49bb5459548af96b69ae2e4
William Narmontas <william@scalawilliam.com>
2015-11-01 14:43:34 +0000

    Try it this way now.

--- build.sbt
+++ build.sbt
@@ -66,7 +66,10 @@ lazy val pingerJournalReader = Project(
   base = file("pinger-journal-reader")
 ).settings(libraryDependencies ++= Seq(
   "com.typesafe.slick" %% "slick" % "3.1.0",
-  "org.scala-lang.modules" %% "scala-async" % "0.9.5"
+  "org.scala-lang.modules" %% "scala-async" % "0.9.5",
+  "org.slf4j" % "slf4j-nop" % "1.6.4",
+  "org.postgresql" % "postgresql" % "9.4-1204-jdbc42",
+  "mysql" % "mysql-connector-java" % "5.1.37"
 ))
 .dependsOn(pingerCore)
 
--- pinger-journal-reader/src/main/scala/ReadGamesApp.scala
+++ pinger-journal-reader/src/main/scala/ReadGamesApp.scala
@@ -1,35 +1,45 @@
 import java.net.URL
 
 import gg.duel.pinger.data.journal.JournalReader
-import slick.driver.H2Driver.api._
+import slick.driver.PostgresDriver.api._
 
 import scala.async.Async
+import scala.concurrent.Future
 
 object ReadGamesApp extends App {
-  val sourceUrl = new URL(args(0))
-  val destinationDatabase = Database.forURL(url = args(1), driver="org.h2.Driver")
-  val journalReader = new JournalReader(sourceUrl)
+
+  val destinationDatabase = Database.forURL(url = args(0), driver = "org.postgresql.Driver")
   val games = TableQuery[Games]
-  val setup = DBIO.seq(games.schema.create)
+
   import scala.concurrent.ExecutionContext.Implicits.global
-  Async.async {
-    println("Trying to save dem stuff.")
-    Async.await(destinationDatabase.run(setup))
-    println("Starting to read games...")
-    journalReader.getGamesIterator.foreach{game =>
-      val id = game.fold(_.startTimeText, _.startTimeText)
-      val json = game.fold(_.toJson, _.toJson)
-      val req = DBIO.seq(games += (id, json))
-      Async.async {
-        println(s"Found game ID $id...")
-        Async.await(destinationDatabase.run(req))
-        println(s"Saved game ID $id.")
+
+  args.tail.par.foreach { file =>
+    val sourceUrl = new URL(file)
+    val journalReader = new JournalReader(sourceUrl)
+    Async.async {
+      Async.await {
+        Future.sequence {
+          journalReader.getGamesIterator.map { game =>
+            val id = game.fold(_.startTimeText, _.startTimeText)
+            val json = game.fold(_.toJson, _.toJson)
+            val req = DBIO.seq(games.insertOrUpdate(id, json))
+            Async.async {
+              println(s"Found game ID $id...")
+              Async.await(destinationDatabase.run(req))
+              println(s"Saved game ID $id.")
+            }
+          }
+        }
       }
     }
+    journalReader.close()
   }
 }
+
 class Games(tag: Tag) extends Table[(String, String)](tag, "GAMES") {
   def id = column[String]("ID", O.PrimaryKey)
+
   def json = column[String]("JSON")
+
   def * = (id, json)
 }
