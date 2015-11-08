464b625371bb1ff9e22cd754a16e76cf3eaeff47
William Narmontas <william@scalawilliam.com>
2015-11-08 10:36:52 +0000

    Travis still not working. Weird.

--- download-resources.sh
+++ download-resources.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 mkdir -p resources
-[ -e GeoLiteCityv6.dat ] || { wget -O - http://geolite.maxmind.com/download/geoip/database/GeoLiteCityv6-beta/GeoLiteCityv6.dat.gz | gunzip - > resources/GeoLiteCityv6.dat; }
-[ -e GeoIPASNumv6.dat ] || { wget -O - http://geolite.maxmind.com/download/geoip/database/asnum/GeoIPASNumv6.dat.gz | gunzip - > resources/GeoIPASNumv6.dat; }
+cd resources/
+[ -e GeoLiteCityv6.dat ] || { wget -O - http://geolite.maxmind.com/download/geoip/database/GeoLiteCityv6-beta/GeoLiteCityv6.dat.gz | gunzip - > GeoLiteCityv6.dat; }
+[ -e GeoIPASNumv6.dat ] || { wget -O - http://geolite.maxmind.com/download/geoip/database/asnum/GeoIPASNumv6.dat.gz | gunzip - > GeoIPASNumv6.dat; }
+ls -lsa
+ls -lsa ..
