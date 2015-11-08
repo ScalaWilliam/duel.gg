889191b24ef8cdb49f7e3137c17028a8b8183c53
William Narmontas <william@scalawilliam.com>
2015-11-08 10:41:27 +0000

    Ah. Has to be in API directory.

--- download-resources.sh
+++ download-resources.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-mkdir -p resources
-cd resources/
+mkdir -p api/resources
+cd api/resources
 [ -e GeoLiteCityv6.dat ] || { wget -O - http://geolite.maxmind.com/download/geoip/database/GeoLiteCityv6-beta/GeoLiteCityv6.dat.gz | gunzip - > GeoLiteCityv6.dat; }
 [ -e GeoIPASNumv6.dat ] || { wget -O - http://geolite.maxmind.com/download/geoip/database/asnum/GeoIPASNumv6.dat.gz | gunzip - > GeoIPASNumv6.dat; }
-ls -lsa
-ls -lsa ..
