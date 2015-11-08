2ce0e56bd9fb03d341b50ee9937fac637de33f29
William Narmontas <william@scalawilliam.com>
2015-11-08 08:14:26 +0000

    Use play-json for ctf, seems to fail tests

--- build.sbt
+++ build.sbt
@@ -42,7 +42,8 @@ lazy val gameEvaluator = Project(
   .settings(libraryDependencies ++= Seq(
     "org.scalatest" %% "scalatest" % "2.2.5" % "test",
     "org.scalactic" %% "scalactic" % "2.2.5",
-    "org.json4s" %% "json4s-native" % "3.3.0"
+    "org.json4s" %% "json4s-native" % "3.3.0",
+    json
   ))
 
 lazy val gamesClojureCore = Project(
--- download-resources.sh
+++ download-resources.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 mkdir -p resources
+cd resources
 [ -e GeoLiteCityv6.dat ] || { wget -O - http://geolite.maxmind.com/download/geoip/database/GeoLiteCityv6-beta/GeoLiteCityv6.dat.gz | gunzip - > resources/GeoLiteCityv6.dat; }
 [ -e GeoIPASNumv6.dat ] || { wget -O - http://geolite.maxmind.com/download/geoip/database/asnum/GeoIPASNumv6.dat.gz | gunzip - > resources/GeoIPASNumv6.dat; }
