13bc3a2162694f4be9a45f50c98756e0d58b47c9
Vykintas Narmontas (William) <william@scalawilliam.com>
2016-07-10 22:13:48 +0800

    Reader to be less strict.

--- pinger-journal/src/main/scala/gg/duel/pinger/ReaderApp.scala
+++ pinger-journal/src/main/scala/gg/duel/pinger/ReaderApp.scala
@@ -1,16 +1,12 @@
 package gg.duel.pinger
 
-import java.nio.file.{Files, Paths}
-
 import gg.duel.pinger.data.journal.JournalReader
 
 object ReaderApp extends App {
 
-  val fah = args.map(arg => Paths.get(arg)).find(p => Files.exists(p)).getOrElse {
-    throw new IllegalArgumentException(s"Could not find a file that exists from arguments ${args.toList}")
-  }
+  val fah = new java.net.URL(args(1))
 
-  val br = new JournalReader(fah.toFile)
+  val br = new JournalReader(fah)
   try {
     val start = System.currentTimeMillis()
     br.getGamesIterator.map(_.toJson).foreach(println)
